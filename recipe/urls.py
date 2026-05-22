cd8eb26e46a2e76e983249d2b5b3c324b1c1832e
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-05-22 16:57:58 +0300

    feat: wire recipe view urls

---
+++ recipe/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('categories/<int:category_id>/', views.category_detail, name='category_detail'),
+]
