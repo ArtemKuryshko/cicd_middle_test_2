1dad76b75145a09e7cda4da06133b0651c251f68
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-05-22 17:12:48 +0300

    test: cover category detail recipes

--- recipe/tests.py
+++ recipe/tests.py
@@ -28,3 +28,29 @@ class RecipeViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main.html')
         self.assertEqual(list(response.context['recipes']), [])
+
+    def test_category_detail_view_returns_recipes_for_category(self):
+        target_category = Category.objects.create(name='Soups')
+        other_category = Category.objects.create(name='Desserts')
+        target_recipe = Recipe.objects.create(
+            title='Borscht',
+            description='Description',
+            instructions='Instructions',
+            ingredients='Ingredients',
+            category=target_category,
+        )
+        Recipe.objects.create(
+            title='Cake',
+            description='Description',
+            instructions='Instructions',
+            ingredients='Ingredients',
+            category=other_category,
+        )
+
+        response = self.client.get(
+            reverse('category_detail', kwargs={'category_id': target_category.id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'category_detail.html')
+        self.assertEqual(list(response.context['category']), [target_recipe])
