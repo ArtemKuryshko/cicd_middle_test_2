73d27214aa5ef9d63261a42026290447bb273bc0
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-05-22 16:56:09 +0300

    feat: add category detail view

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,15 @@
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Recipe
+from .models import Category, Recipe
 
 
 def main(request):
     recipes = Recipe.objects.order_by('?')[:10]
     return render(request, 'main.html', {'recipes': recipes})
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    recipes = category.categories.all()
+    return render(request, 'category_detail.html', {'category': recipes})
