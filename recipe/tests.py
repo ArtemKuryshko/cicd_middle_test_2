1f5cd024b08660d724fd2c7be09a6e1a88aa39c7
Artem Kuryshko <kuryshkoartem05@gmail.com>
2026-05-22 17:03:59 +0300

    test: cover main view recipe limit

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Category, Recipe
+
+
+class RecipeViewTests(TestCase):
+    def test_main_view_returns_ten_random_recipes(self):
+        category = Category.objects.create(name='Breakfast')
+        for number in range(12):
+            Recipe.objects.create(
+                title=f'Recipe {number}',
+                description='Description',
+                instructions='Instructions',
+                ingredients='Ingredients',
+                category=category,
+            )
+
+        response = self.client.get(reverse('main'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertEqual(len(response.context['recipes']), 10)
